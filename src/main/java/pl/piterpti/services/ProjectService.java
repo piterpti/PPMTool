5d5c6f1779fee877c448defe12423aeca070c9af
Piter <piterpti@gmail.com>
2018-12-02 12:31:38 +0100

    CRUD finished for Project entity

--- src/main/java/pl/piterpti/services/ProjectService.java
+++ src/main/java/pl/piterpti/services/ProjectService.java
@@ -33,5 +33,20 @@ public class ProjectService {
 		
 		return project;
 	}
+	
+	public Iterable<Project> findAllProjects() {
+		return projectRepository.findAll();
+	}
+	
+	public void deleteProjectByIdentifier(String projectId) {
+		Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+		
+		if (project == null) {
+			throw new ProjectIdException(
+					"Cannot delete Project with ID '" + projectId.toUpperCase() + "'. This project does not exist");
+		}
+		
+		projectRepository.delete(project);
+	}
 
 }
