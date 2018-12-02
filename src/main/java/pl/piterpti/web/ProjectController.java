8815eee5474fbd0e7074b33db142ccdea948c4e4
Piter <piterpti@gmail.com>
2018-12-02 11:11:00 +0100

    Project entity validation

--- src/main/java/pl/piterpti/web/ProjectController.java
+++ src/main/java/pl/piterpti/web/ProjectController.java
@@ -1,8 +1,11 @@
 package pl.piterpti.web;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,7 +28,11 @@ public class ProjectController {
 	private ProjectService projectService;
 	
 	@PostMapping("")
-	public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
+	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
+		
+		if (result.hasErrors()) {
+			return new ResponseEntity<String>("Invalid Project object", HttpStatus.BAD_REQUEST);
+		}
 		
 		project = projectService.saveOrUpdateProject(project);
 		return new ResponseEntity<Project>(project, HttpStatus.OK);
