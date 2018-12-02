3ee42e250537beda53bdc194d71a3b9671c2972b
Piter <piterpti@gmail.com>
2018-12-02 09:57:59 +0100

    First commit

---
+++ src/main/java/pl/piterpti/PpmToolApplication.java
@@ -0,0 +1,12 @@
+package pl.piterpti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmToolApplication.class, args);
+	}
+}
