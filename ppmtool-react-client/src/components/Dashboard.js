7106f2bdb23c52b589f4efa68dbb4d6d2effa564
Piter <piterpti@gmail.com>
2018-12-05 15:39:56 +0100

    Added layout for header, dashboard & project item. Added font awesome link

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -5,12 +5,23 @@ import ProjectItem from './Project/ProjectItem';
 export class Dashboard extends Component {
   render() {
     return (
-    <div>
-      <h1 className="alert alert-warning">Welcome to the Dashboard</h1>
-      <ProjectItem/>
-      <ProjectItem/>
-      <ProjectItem/>
-      </div>
+      <div className="projects">
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12">
+                    <h1 className="display-4 text-center">Projects</h1>
+                    <br />
+                    <a href="ProjectForm.html" className="btn btn-lg btn-info">
+                        Create a Project
+                    </a>
+                    <br />
+                    <hr />
+
+                    <ProjectItem/>
+                </div>
+            </div>
+        </div>
+    </div>
     )
   }
 }
