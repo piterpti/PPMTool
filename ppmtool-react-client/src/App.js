5b7133de6f0b158859295863110a2b0fd0701e87
Piter <piterpti@gmail.com>
2018-12-05 15:53:09 +0100

    React code formated :)

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
-import './App.css';
-import { Dashboard } from './components/Dashboard';
-import Header from './components/Layout/Header';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from "react";
+import "./App.css";
+import { Dashboard } from "./components/Dashboard";
+import Header from "./components/Layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header/>
-        <Dashboard/>
+        <Header />
+        <Dashboard />
       </div>
     );
   }
