3e6a6987e4fa159e665934f3d9333599809e6144
Piter <piterpti@gmail.com>
2018-12-16 13:23:13 +0100

    Adding project from react

--- ppmtool-react-client/src/components/Project/AddProject.js
+++ ppmtool-react-client/src/components/Project/AddProject.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { createProject } from "../../actions/projectActions";
 
 class AddProject extends Component {
   constructor() {
@@ -17,7 +20,7 @@ class AddProject extends Component {
   }
 
   onChange(e) {
-    this.setState({[e.target.name]:e.target.value});
+    this.setState({ [e.target.name]: e.target.value });
   }
 
   onSubmit(e) {
@@ -29,9 +32,9 @@ class AddProject extends Component {
       description: this.state.description,
       startDate: this.state.startDate,
       endDate: this.state.endDate
-    }
+    };
 
-    console.log(newProject);
+    this.props.createProject(newProject, this.props.history);
   }
 
   render() {
@@ -107,4 +110,11 @@ class AddProject extends Component {
   }
 }
 
-export default AddProject;
+AddProject.propTypes = {
+  createProject: PropTypes.func.isRequired
+};
+
+export default connect(
+  null,
+  { createProject }
+)(AddProject);
